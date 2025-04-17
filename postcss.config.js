3b07bcb620a325b3ba7e493d677724c8998f5359
gichigi <gichigi@me.com>
2025-04-17 17:23:25 +0100

    Add explicit PostCSS configuration for better CSS processing

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true
+      }
+    }
+  }
+} 
